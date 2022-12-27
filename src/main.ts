d6db2ad50c77eb63d68ba2530157e866090ecb57
Vitalii Dykhal <99541944+byht1@users.noreply.github.com>
2022-12-27 20:47:55 +0200

    prot

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.service';
+
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule, { cors: true });
+
+    const config = new DocumentBuilder()
+      .setTitle('Bakery')
+      .setDescription('Документація REST API')
+      .setVersion('0.0.1')
+      .addServer('http://localhost:5000')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/docs', app, document);
+
+    await app.listen(PORT, () =>
+      console.log(`server start http://localhost:${PORT}`),
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// gs://music-db-11801.appspot.com
+
+start();
