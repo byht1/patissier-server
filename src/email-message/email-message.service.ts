8b5cfc171d5089d58ad27b1f952c1ace17832e95
Vitalii Dykhal <99541944+byht1@users.noreply.github.com>
2022-12-30 21:51:43 +0200

    dev

--- src/email-message/email-message.service.ts
+++ src/email-message/email-message.service.ts
@@ -17,7 +17,9 @@ export class EmailMessageService {
     <span>Для підтвердження перейдіть  <a href="${dev}/user/activate/${link}">за посиланням</a></span>
     </div>`;
     try {
-      await this.example(html, title, email);
+      if (!dev) {
+        await this.example(html, title, email);
+      }
 
       return link;
     } catch (error) {
