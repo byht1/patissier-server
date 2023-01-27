8292a440f89fa835089b723053dd9824f3404da8
Vitalik Dykhal <vitalikbyht1@gmail.com>
2023-01-27 20:37:54 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ const start = async () => {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule, { cors: true });
 
-    // const config = new DocumentBuilder()
-    //   .setTitle('Bakery')
-    //   .setDescription('Documentation REST API')
-    //   .setVersion('1.0.11')
-    //   .addServer(`http://localhost:${PORT}`)
-    //   .build();
-    // const document = SwaggerModule.createDocument(app, config);
-    // SwaggerModule.setup('/docs', app, document);
+    const config = new DocumentBuilder()
+      .setTitle('Bakery')
+      .setDescription('Documentation REST API')
+      .setVersion('1.0.11')
+      .addServer(`http://localhost:${PORT}`)
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/docs', app, document);
 
     await app.listen(PORT, () =>
       console.log(`server start http://localhost:${PORT}/docs`),
